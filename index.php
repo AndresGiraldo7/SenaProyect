ebcebc3a4876add14d86b7c1aedb0ab9d233f07b
AndresGiraldo7 <and.th420@gmail.com>
2022-04-13 20:25:09 -0500

    Primer Commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <title>Control De Vencidos</title>
+</head>
+<body>
+    <h1>INICIO DE SESION<h1>
+    <h2>Bienvenida/o - Sistema Para El Contror Vencidos</h2>
+    <form class = "login" method="POST" action ="">
+    Usuario<br>
+    <input type="text" name="Usuario" required="requerido" autocomplete="off" placeholder="Usuario"><br><br>
+    Contraseña<br>
+    <input type="text" name="Contrasena" required="requerido" autocomplete="off" placeholder="Contraseña"><br><br>
+    <input type="submit" value="Iniciar Sesion">
+    </form>
+</body>
+</html>
